751c5db0e8dc1582e36979cc79dacccb4017fea0
Olivia Pomeroy <opomeroy26@gmail.com>
2022-04-05 10:41:03 -0700

    commit tree 0b58bad8348d84fd088d18a20365aab3f24958a1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,20 @@
 class MoviesController < ApplicationController
+
+  def create
+    # byebug
+    movie = Movie.create(movie_params)
+    render json: movie
+  end
   
   def index
     movies = Movie.all
     render json: movies
+  end
+
+  private 
+  
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
   end
 
 end
